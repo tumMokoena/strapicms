24e52ae2632ea6511cf9dcc9c7dc15835d983fcc
tumMokoena <61909721+tumMokoena@users.noreply.github.com>
2020-12-02 06:33:11 +0200

    ..

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      "cloud_name": "clickifylesotho",
+        "api_key": "127633125723183",
+        "api_secret": "SnNdKP4JFU0HyxDXlp420AtzX98"
+    },
+  },
+  // ...
+});
